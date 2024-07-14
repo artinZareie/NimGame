7a3911be75dbb7e3430570787bda855b3b34fd43
Artin Zarei <artin.zareie1383@gmail.com>
2024-07-14 21:49:40 +0330

    Add SDL and SQLite

--- headers/Constants.h
+++ headers/Constants.h
@@ -1,3 +1,8 @@
 #pragma once
-const int MaxPieces = 10;
-const int NoPiles = 5;
+
+const int MAX_PIECES = 10;
+const int NO_PILES = 5;
+const int SCREEN_WDITH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const char * const APP_NAME = "Nim Game";
